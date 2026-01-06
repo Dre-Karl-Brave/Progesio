d4b8f9ef9ee9f6009a99a3cd95b8680a545462ab
JPAVictoria <jpavictoria829@gmail.com>
2026-01-06 23:07:51 +0800

    feat: file structure and stuffs

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
 export default function Home() {
-  return <h1>Andrepogi</h1>
+  return <h1>Andrepogi</h1> // Don't use this as entry point,
+  //  just create a button leading to progresio/login (entry point for actual system)
 }
