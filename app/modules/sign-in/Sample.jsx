d59383809c2d8b00305d852427c11cee7457fa9f
JPAVictoria <jpavictoria829@gmail.com>
2026-01-07 21:38:13 +0800

    feat: finalize structure

---
+++ app/modules/sign-in/Sample.jsx
@@ -0,0 +1,17 @@
+//this is where you put your components karl para import import nalang sa page.
+
+// modules = components
+// components == reusable shits (not specific)
+
+{
+  /** Karl do this:
+  
+  no need to use next auth, just custom jwt like the usual
+  Background: #FFFFFF (pure white)
+Surface / Cards: #F8FAFC (very light gray)
+Border / Divider: #E5E7EB
+Primary Text: #0F172A (almost-black navy)
+Secondary Text: #475569
+
+  */
+}
